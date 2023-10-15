4e4a55754a128661b71406698fca56cb12520803
Shahriar Hossein <shahriar2507@gmail.com>
2023-10-15 20:46:41 +0600

    modified devise

--- db/schema.rb
+++ db/schema.rb
@@ -6,11 +6,11 @@
 # db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use exte\rnal dependencies or application code.
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_135218) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_15_112116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_135218) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "lastName"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
