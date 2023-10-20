cb1669f1b7cef0ae5e586faa78c4ce2cb3490119
Alaminplabon <donplabon94@gmail.com>
2023-10-20 22:23:00 +0600

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 # this is the model for post
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :picture
   has_many :comments, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true, length:{
     minimum: 5
+
   }
 end
